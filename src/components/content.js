c8cc47feca7b024e72dea68540eef2c1846ce8ce
Araliatrix <agentisaacson@gmail.com>
2018-08-07 17:21:36 -0600

    added logo and contact us component and styled navbar

--- src/components/content.js
+++ src/components/content.js
@@ -6,6 +6,8 @@ import StoreForm from './stores/store-form';
 import RepForm from './reps/rep-form';
 import Spacescape from './stores/pages/spacescape';
 import Coolness from './stores/pages/coolness';
+import Contact from './contact-us';
+import About from './about-us';
 
 
 export default function() {
@@ -13,6 +15,8 @@ export default function() {
     <div className="content">
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/about-us" component={About} />
+        <Route exact path="/contact-us" component={Contact} />
         <Route exact path="/rep-form" component={RepForm} />
         <Route exact path="/store-form" component={StoreForm} />
         <Route exact path="/stores/spacescape" component={Spacescape} />
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,17 +6,40 @@ export default function() {
 		<div className="navbar">
 			<div className="link">
 				<NavLink className={"logo"} exact to="/">
-					<span><img src="D:\Nutshell\code\logos\Nutshell-logos\2x\nutshell-logo-orange@2x.png"/></span>
+					<div>
+						<img src="/assets/logos\Nutshell-logos\2x\nutshell-logo-orange@2x.png" />
+						<p>Your New Gatekeeper</p>
+					</div>
 				</NavLink>
 			</div>
 			<div className="link">
-				<NavLink className={"navlink"} activeClassName={"activeNavlink"} exact to="/">
+				<NavLink
+					className={"navlink"}
+					activeClassName={"activeNavlink"}
+					exact
+					to="/"
+				>
 					Homepage
 				</NavLink>
 			</div>
 			<div className="link">
-				<NavLink className={"navlink"} activeClassName={"activeNavlink"} exact to="/about-us">
+				<NavLink
+					className={"navlink"}
+					activeClassName={"activeNavlink"}
+					exact
+					to="/about-us"
+				>
 					About Us
+				</NavLink>
+			</div>
+			<div className="link">
+				<NavLink
+					className={"navlink"}
+					activeClassName={"activeNavlink"}
+					exact
+					to="/contact-us"
+				>
+					Contact Us
 				</NavLink>
 			</div>
 		</div>
